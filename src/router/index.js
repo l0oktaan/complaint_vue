5483c9176a2078967a72d679d551ec26ca8fc333
Jeedev <jijiswta18@gmail.com>
2023-05-08 11:38:07 +0700

    edit router

--- src/router/index.js
+++ src/router/index.js
@@ -8,59 +8,48 @@ import LayoutView from "../views/users/LayoutView.vue"
 import MainView from "../views/users/main/MainView.vue"
 import ContactView from "../views/users/contact/ContactView.vue"
 import UserLoginView from '../views/users/LoginView.vue'
+import FollowView from '../views/users/follow/FollowView.vue'
+import FollowDetail from '../views/users/follow/FollowDetail.vue'
+import ComplainView from '../views/users/complain/ComplainView.vue'
+import EditProfile from '../views/EditProfile.vue'
 
 import ForgotPasswordView from '../views/users/ForgotPassword.vue'
 import ResetPasswordView from '../views/users/ResetPassword.vue'
-import ComplainView from '../views/users/complain/ComplainView.vue'
-import FollowView from '../views/users/follow/FollowView.vue'
-import FollowDetail from '../views/users/follow/FollowDetail.vue'
+
+
 
 
 // backoffice
 import LoginView from '../views/backoffice/LoginView.vue'
 import ReceiveComplain from "../views/backoffice/receive/ReceiveView.vue"
 import ReceiveDetail from "../views/backoffice/receive/ReceiveDetail.vue"
+import BackofficeFollowView from '../views/backoffice/follow/FollowView.vue'
+import BackofficeFollowDetail from '../views/backoffice/follow/FollowDetail.vue'
 import PersonnelView from '../views/backoffice/personnel/UserViews.vue'
 import PersonnelForm from '../views/backoffice/personnel/UserForm.vue'
 import PersonnelFormEdit from '../views/backoffice/personnel/UserForm.vue'
-import BackofficeFollowView from '../views/backoffice/follow/FollowView.vue'
-import BackofficeFollowDetail from '../views/backoffice/follow/FollowDetail.vue'
-import ReportView from '../views/backoffice/report/ReportView.vue'
 import RegisterComplain from '../views/backoffice/register/RegisterViews.vue'
 import Registerform from '../views/backoffice/register/RegisterForm.vue'
+// import ReportView from '../views/backoffice/report/ReportView.vue'
+
 
 import HomeView from '../views/HomeView.vue'
-import AboutView from '../views/AboutView.vue'
+import HomeUserView from '../views/HomeView.vue'
+// import AboutView from '../views/AboutView.vue'
 
-import EditProfile from '../views/EditProfile.vue'
+
 
 
 
 Vue.use(VueRouter)
 
+
 const routes = [
 
-
-  // backoffice
-  {
-    path: '/backoffice/login',
-    name: 'login',
-    component: LoginView,
-    // beforeEnter (to, from, next) {
-    //   store.dispatch('checkLogin')
-    //   if (store.state.user) {         
-    //     next({ name: 'about' })
-    //   } else {
-    //       next()
-    //   }
-    // },
-  },
-
-  // user
   {
     path: '/',
     component: LayoutView,
-
+    
     children: [
       {
         path: '/',
@@ -71,20 +60,6 @@ const routes = [
         path: '/user/contact',
         name: 'contact',
         component: ContactView,
-      },
-
-      {
-        path: '/user/login',
-        name: 'userLogin',
-        component: UserLoginView,
-        // beforeEnter (to, from, next) {
-        //   store.dispatch('checkLogin')
-        //   if (store.state.user) {         
-        //     next({ name: 'complaint' })
-        //   } else {
-        //       next()
-        //   }
-        // },
       },
     ]
   },
@@ -103,57 +78,35 @@ const routes = [
   },
 
   {
+    path: '/backoffice/login',
+    name: 'login',
+    component: LoginView
+  },
+  { 
     path: '/',
     component: HomeView,
     beforeEnter (to, from, next) {
       store.dispatch('checkLogin')
-      if (store.state.user) {     
+      if (store.state.user.roles != 'user') {   
           next()          
-      } else {
-        if(store.state.user == 'user'){
-          console.log('========>','user');
-          next('/user/login')
-        }
+      }else{
+        next({ name: 'login' })
       }
     },
 
     children: [
-      {
-        path: '/user/report',
-        name: 'complain-report',
-        component: ComplainView,
-      },
-      {
-        path: '/user/follow/detail/:id',
-        name: 'complain-detail',
-        component: FollowDetail,
-      },
-      {
-        path: '/user/follow',
-        name: 'complain-follow',
-        component: FollowView,
-      },
-     
-      {
-        path: '/user/profile',
-        name: 'profile',
-        component: EditProfile,
-      },
-
       {
         path: '/backoffice/receive',
         name: 'receive',
         component: ReceiveComplain,
         
       },
-
       {
         path: '/backoffice/receive/detail/:id',
         name: 'receive-detail',
         component: ReceiveDetail,
         
       },
-
       {
         path: '/backoffice/follow',
         name: 'backoffice-follow',
@@ -165,9 +118,6 @@ const routes = [
         name: 'backoffice-complaindetail',
         component: BackofficeFollowDetail,
       },
-
-
-
       {
         path: '/backoffice/personnel',
         name: 'personnel',
@@ -195,22 +145,248 @@ const routes = [
         component: Registerform
       },
 
-      
+    ]
+
+  },
+
+  {
+    path: '/user/login',
+    name: 'userLogin',
+    component: UserLoginView
+  },
+
+  { 
+    path: '/',
+    component: HomeUserView,
+    beforeEnter (to, from, next) {
+      store.dispatch('checkLogin')
+      if (store.state.user.roles == 'user') {   
+        next()          
+      }else{
+        next({ name: 'userLogin' })
+      }
+    },
+
+    children: [
       {
-        path: '/backoffice/report',
-        name: 'report_view',
-        component: ReportView
+        path: '/user/profile',
+        name: 'profile',
+        component: EditProfile,
       },
 
       {
-        path: '/user/testPdf',
-        name: 'about',
-        component: AboutView,
-        
-      }
+        path: '/user/follow',
+        name: 'complain-follow',
+        component: FollowView,
+      },
+      {
+        path: '/user/follow/detail/:id',
+        name: 'complain-detail',
+        component: FollowDetail,
+      },
+
+      {
+        path: '/user/report',
+        name: 'complain-report',
+        component: ComplainView,
+      },
+    
     ]
+
   },
+ 
 ]
+
+// const routes = [
+
+
+//   // backoffice
+//   {
+//     path: '/backoffice/login',
+//     name: 'login',
+//     component: LoginView,
+//     beforeEnter (to, from, next) {
+//       store.dispatch('checkLogin')
+//         if (store.state.user) {    
+//           console.log('=========', store.state.user);     
+//         next({ name: 'receive' })
+//       } else {
+//         console.log('1111');     
+//           next()
+//       }
+//     },
+//   },
+
+//   // user
+//   {
+//     path: '/',
+//     component: LayoutView,
+
+//     children: [
+//       {
+//         path: '/',
+//         name: 'main',
+//         component: MainView,
+//       },
+//       {
+//         path: '/user/contact',
+//         name: 'contact',
+//         component: ContactView,
+//       },
+
+//       {
+//         path: '/user/login',
+//         name: 'userLogin',
+//         component: UserLoginView,
+//         // beforeEnter (to, from, next) {
+//         //   store.dispatch('checkLogin')
+//         //   if (store.state.user) {         
+//         //     next({ name: 'complaint' })
+//         //   } else {
+//         //       next()
+//         //   }
+//         // },
+//       },
+//     ]
+//   },
+
+//   {
+//     path: '/user/forgot-password',
+//     name: 'forgot-password',
+//     component: ForgotPasswordView,
+    
+//   },
+//   {
+//     path: '/user/reset-password',
+//     name: 'reset-password',
+//     component: ResetPasswordView,
+    
+//   },
+
+//   {
+//     path: '/',
+//     component: HomeView,
+//     // beforeEnter (to, from, next) {
+//     //   store.dispatch('checkLogin')
+//     //   if (store.state.user) {   
+//     //     console.log('========>','test');  
+//     //       next()          
+//     //   } else {
+//     //     if(store.state.user == 'user'){
+//     //       console.log('========>','user');
+//     //       next('/user/login')
+//     //     }
+//     //   }
+//     // },
+
+
+//     // beforeEnter (to, from, next) {
+//     //   store.dispatch('checkLogin')
+//     //   if (store.state.user == 'user') {     
+//     //     console.log('user');
+//     //     next('/user/login')
+                  
+//     // }else if(store.state.user != 'user'){
+//     //     console.log('admin');
+//     //     next('/backoffice/login')
+//     // }else {
+//     //     next()  
+//     //   }
+//     // },
+//     children: [
+//       {
+//         path: '/user/report',
+//         name: 'complain-report',
+//         component: ComplainView,
+//       },
+//       {
+//         path: '/user/follow/detail/:id',
+//         name: 'complain-detail',
+//         component: FollowDetail,
+//       },
+//       {
+//         path: '/user/follow',
+//         name: 'complain-follow',
+//         component: FollowView,
+//       },
+     
+//       {
+//         path: '/user/profile',
+//         name: 'profile',
+//         component: EditProfile,
+//       },
+
+//       {
+//         path: '/backoffice/receive',
+//         name: 'receive',
+//         component: ReceiveComplain,
+        
+//       },
+
+//       {
+//         path: '/backoffice/receive/detail/:id',
+//         name: 'receive-detail',
+//         component: ReceiveDetail,
+        
+//       },
+
+//       {
+//         path: '/backoffice/follow',
+//         name: 'backoffice-follow',
+//         component: BackofficeFollowView,
+//       },
+
+//       {
+//         path: '/backoffice/follow/detail/:id',
+//         name: 'backoffice-complaindetail',
+//         component: BackofficeFollowDetail,
+//       },
+
+
+
+//       {
+//         path: '/backoffice/personnel',
+//         name: 'personnel',
+//         component: PersonnelView,
+        
+//       },
+//       {
+//         path: '/backoffice/personnel/form',
+//         name: 'personnel_form',
+//         component: PersonnelForm
+//       },
+//       {
+//         path: '/backoffice/personnel/form/:id',
+//         name: 'personnel_formedit',
+//         component: PersonnelFormEdit
+//       },
+//       {
+//         path: '/backoffice/register',
+//         name: 'register_complain',
+//         component: RegisterComplain
+//       },
+//       {
+//         path: '/backoffice/register/form/:id',
+//         name: 'register_form',
+//         component: Registerform
+//       },
+
+      
+//       {
+//         path: '/backoffice/report',
+//         name: 'report_view',
+//         component: ReportView
+//       },
+
+//       {
+//         path: '/user/testPdf',
+//         name: 'about',
+//         component: AboutView,
+        
+//       }
+//     ]
+//   },
+// ]
 
 const router = new VueRouter({
   mode: 'history',
