947442c33585fe43cde66fbf2fcdae577783fb7e
Jeedev <jijiswta18@gmail.com>
2023-05-09 09:46:20 +0700

    update time

--- src/components/detailComplain.vue
+++ src/components/detailComplain.vue
@@ -372,8 +372,8 @@ export default {
             // this.data.start_date  = await moment(response.data.data[0].start_date).add(543, 'year').tz("Asia/Bangkok").locale('th').format('DD MMMM YYYY')
             // this.data.end_date    = await moment(response.data.data[0].end_date).add(543, 'year').tz("Asia/Bangkok").locale('th').format('DD MMMM YYYY')
 
-                 this.data.start_time  = await moment(response.data.data[0].start_date).format('HH:mm')
-                this.data.end_time    = await moment(response.data.data[0].end_date).format('HH:mm')
+                 this.data.start_time  = await moment(response.data.data[0].start_date).tz("Asia/Bangkok").format('HH:mm')
+                this.data.end_time    = await moment(response.data.data[0].end_date).tz("Asia/Bangkok").format('HH:mm')
       
           this.files            = await response.data.data_files
           
