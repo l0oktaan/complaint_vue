b71848ce2ebf366c1ec21596ed5467c8d117b4cd
Jeedev <jijiswta18@gmail.com>
2023-08-04 13:52:47 +0700

    ีupdate sidebar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,31 @@
 <template>
   <v-app id="inspire">
-    <h1>hhhh</h1>
-  
-    <v-navigation-drawer app v-model="drawer" class="sidebar">
-      <v-list-item >
-        <v-avatar  size="50">
-        <img 
-            src="@/assets/images/logo-yellow.png"
-            alt="logo"
-        />
-      </v-avatar>
-          <v-list-item-title class="head">
-            ระบบรับเรื่องร้องเรียนทุจริต
-          </v-list-item-title>
-      </v-list-item>
-      <v-list 
-        class="mt-5"
-        dense 
-        nav
-      >  
+    <v-navigation-drawer 
+      class="sidebar"
+      v-model="drawer"
+      app
+    >
 
-      <router-link 
+      <!-- <v-list-item >
+        <v-avatar  size="50">
+          <img src="@/assets/images/logo-yellow.png" alt="logo"/>
+        </v-avatar>
+          <v-list-item-title class="head"> ระบบรับเรื่องร้องเรียนทุจริต </v-list-item-title>
+      </v-list-item> -->
+
+      <v-list-item >
+        <v-list-item-content class="head-menuhome">
+          <div class="logo-cgd mr-2">
+            <img src="@/assets/images/logo-yellow.png" alt="logo"/>
+          </div>
+          <v-list-item-title class="text-head">
+            รับเรื่องร้องเรียนทุจริต
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-list class="mt-5" dense nav>  
+        <router-link 
           v-if="check_roles.roles === 'user'"
           class="d-flex align-center menu-link"
           active-class="activemenu"
@@ -36,7 +41,6 @@
           </v-list-item>
         </router-link>
 
-        
         <router-link 
           v-if="check_roles.roles === 'user'"
           class="d-flex align-center menu-link"
@@ -52,46 +56,6 @@
             </v-list-item-content>
           </v-list-item>
         </router-link>
-
-        <!-- <router-link 
-          v-if="check_roles.roles === 'user'"
-          class="d-flex align-center menu-link"
-          active-class="activemenu"
-          to="/user/complain"
-        > -->
-        
-
-        <!-- <router-link 
-          v-if="check_roles.roles === 'admin'"
-          class="d-flex align-center menu-link"
-          active-class="activemenu"
-          to="/backoffice/complaint"
-        >
-          <v-list-item link>
-            <i class="fa-solid fa-file menu-icon"></i>
-            <v-list-item-content>
-              <v-list-item-title class="menu-text">
-                เรื่องร้องเรียน
-              </v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </router-link>
-
-        <router-link 
-          v-if="check_roles.roles === 'admin'"
-          class="d-flex align-center menu-link"
-          active-class="activemenu"
-          to="/backoffice/search"
-        >
-          <v-list-item link>
-            <i class="fa-solid fa-magnifying-glass menu-icon"></i>
-            <v-list-item-content>
-              <v-list-item-title class="menu-text">
-                ค้นหาเรื่องร้องเรียน
-              </v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </router-link> -->
 
         <router-link 
           v-if="check_roles.roles === 'admin' || check_roles.roles === 'general'"
@@ -125,23 +89,6 @@
           </v-list-item>
         </router-link>
 
-
-        <!-- <router-link 
-          v-if="check_roles.roles === 'admin'"
-          class="d-flex align-center menu-link"
-          active-class="activemenu"
-          to="/backoffice/report"
-        >
-          <v-list-item link>
-            <i class="fa-solid fa-pen-to-square menu-icon"></i>
-            <v-list-item-content>
-              <v-list-item-title class="menu-text">
-                รายงาน
-              </v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </router-link> -->
-
         <router-link 
           v-if="check_roles.roles === 'admin'"
           class="d-flex align-center menu-link"
@@ -150,7 +97,6 @@
         >
           <v-list-item link>
             <i class="fas fa-users menu-icon"></i>
-            <!-- <i class="fa-solid fa-magnifying-glass menu-icon"></i> -->
             <v-list-item-content>
               <v-list-item-title class="menu-text">
                 รายการบุคลากร
@@ -167,7 +113,6 @@
         >
           <v-list-item link>
             <i class="fa-solid fa-user menu-icon"></i>
-            <!-- <i class="fa-solid fa-magnifying-glass menu-icon"></i> -->
             <v-list-item-content>
               <v-list-item-title class="menu-text">
                 รายการผู้ร้องเรียน
@@ -175,98 +120,152 @@
             </v-list-item-content>
           </v-list-item>
         </router-link>
+<!-- 
+        <router-link 
+          v-if="check_roles.roles === 'admin'"
+          class="d-flex align-center menu-link"
+          active-class="activemenu"
+          to="/backoffice/report"
+        >
+          <v-list-item link>
+            <i class="fa-solid fa-file-export menu-icon"></i>
+            <v-list-item-content>
+              <v-list-item-title class="menu-text">
+                รายงาน
+              </v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </router-link> -->
       </v-list>
 
     </v-navigation-drawer>
-
       <v-app-bar app class="navbar">
+        <div class="style-navbar">
+       
         <v-app-bar-nav-icon class="navbar-icon" @click="drawer = !drawer"></v-app-bar-nav-icon>
-
         <v-spacer></v-spacer>
-      
-        <!-- <UserProfile/> -->
 
-
-        <v-menu
+        <!-- <v-menu
           bottom
           min-width="200px"
           rounded
           offset-y
-        >
-          <template v-slot:activator="{ on }">
-            <v-btn icon x-large v-on="on">
+        > -->
+        <span class="mr-2 color-primary">{{check_roles.name}} {{ check_roles.lastname }}</span>
+        
+        <v-menu offset-y>
+          <template v-slot:activator="{ on, attrs }">
+
+            <div 
+              class="box-user mr-2"
+              v-bind="attrs"
+              v-on="on"
+            >
+              <i class="fa-solid fa-circle-user f-24"></i>
+            </div>
+            <!-- <v-btn icon x-large v-on="on">
               <v-avatar  color="brown" size="36">
                 <i class="fa-solid fa-user"></i>
               </v-avatar>
-              <!-- <v-avatar color="brown" size="36">
-                
-                <i class="fa-solid fa-user"></i>
-
-              </v-avatar> -->
-            </v-btn>
+            </v-btn> -->
           </template>
-          <v-card>
+
+          <v-list>
+
+            <!-- <v-list-item >
+              <v-list-item-title>  
+                <h3> {{ check_roles.name }} {{ check_roles.lastname }} </h3>
+                <p class="text-caption mt-1"> {{ check_roles.email }} </p>
+                <v-divider class="my-3"></v-divider>
+              </v-list-item-title>
+            </v-list-item> -->
+
+            <v-list-item >
+              <v-list-item-title>
+                <v-btn depressed rounded text @click="editProfile"> แก้ไขโปรไฟล์ </v-btn>
+                <v-divider  class="my-3"></v-divider> 
+                <!-- <v-btn
+                  v-if="check_roles.roles == 'user'"
+                  depressed
+                  rounded
+                  text
+                  @click="opensumone()"
+                >
+                  เปลี่ยนรหัสผ่าน
+                </v-btn>
+                <v-divider v-if="check_roles.roles != 'user'" class="my-3"></v-divider> -->
+              
+              </v-list-item-title>
+            </v-list-item>
+
+            <v-list-item v-if="check_roles.roles == 'user'">
+              <v-list-item-title>
+                <!-- <v-btn depressed rounded text @click="editProfile"> แก้ไขโปรไฟล์ </v-btn>
+                <v-divider v-if="check_roles.roles == 'user'" class="my-3"></v-divider>  -->
+                <v-btn
+                  
+                  depressed
+                  rounded
+                  text
+                  @click="opensumone()"
+                >
+                  เปลี่ยนรหัสผ่าน
+                </v-btn>
+                <v-divider class="my-3"></v-divider>
+              
+              </v-list-item-title>
+            </v-list-item>
+
+            <v-list-item >
+              <v-list-item-title>
+                <v-btn depressed rounded text @click="logout"> <i class="fa-solid fa-arrow-right-from-bracket mr-1"></i> ออกจากระบบ </v-btn>
+              </v-list-item-title>
+            </v-list-item>
+          </v-list>
+
+          <!-- <v-card>
             <v-list-item-content class="justify-center">
               <div class="mx-auto text-center">
-                <!-- <v-avatar color="brown">
-                  <span class="white--text text-h5">{{ check_roles.initials }}</span>
-                </v-avatar> -->
                 <h3>{{ check_roles.name }} {{ check_roles.lastname }}</h3>
                 <p class="text-caption mt-1">
                   {{ check_roles.email }}
                 </p>
                 <v-divider class="my-3"></v-divider>
 
-                <v-btn depressed rounded text @click="editProfile"> 
-                  แก้ไขโปรไฟล์
-                </v-btn>
-                <v-divider v-if="check_roles.roles == 'user'" class="my-3"></v-divider>
-               
+                <v-btn depressed rounded text @click="editProfile"> แก้ไขโปรไฟล์ </v-btn>
+                <v-divider v-if="check_roles.roles == 'user'" class="my-3"></v-divider> 
                 <v-btn
                   v-if="check_roles.roles == 'user'"
                   depressed
                   rounded
                   text
                   @click="opensumone()"
-
                 >
                   เปลี่ยนรหัสผ่าน
                 </v-btn>
                 <v-divider v-if="check_roles.roles != 'user'" class="my-3"></v-divider>
-               
-                <!-- <v-btn
-                  v-if="check_roles.roles != 'user'"
-                  depressed
-                  rounded
-                  text
-                  @click="homeUser()"
-
-                >
-                  หน้าบ้าน
-                </v-btn> -->
+              
               </div>
             </v-list-item-content>
-          </v-card>
+          </v-card> -->
+
         </v-menu>
 
         <DialogsResetPassword ref="sum" :userId="check_roles.id" />
 
-        <button @click="logout" id="logout"><i class="fa-solid fa-arrow-right-from-bracket"></i> Logout</button>
-    </v-app-bar>
+        <!-- <button @click="logout" id="logout"><i class="fa-solid fa-arrow-right-from-bracket"></i> Logout</button> -->
+      </div>
+      </v-app-bar>
     <v-main class="main">
       <v-container>
-        <!-- <div class="style-page"> -->
           <router-view></router-view>
-        <!-- </div> -->
       </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-  // import UserProfile from '@/components/userProfile.vue';
 import DialogsResetPassword from '@/components/dialogsResetPassword.vue';
-// import SideBar from '@/components/main/sideBar.vue';
 import store from '../store/index.js';
   export default {
     name: "MyHome",
@@ -284,21 +283,13 @@ import store from '../store/index.js';
         this.$router.push("/user/login");
       },
 
-        // resetPassword() {
-        //     this.dialog = true
-        //     console.log(this.dialog);
-        //     // this.$router.push({ name: "reset-password" });
-        // },
-        async editProfile(){
-          if(this.check_roles.roles === 'user'){
-            await this.$router.push("/user/profile").catch(()=>{});
-          }else{
-            // await this.$router.push("/backoffice/profile").catch(()=>{});
-            this.$router.push({ name: 'personnel_formedit', params: { id: this.check_roles.id },})
-          }
-    
-          // await this.$router.push("/user/profile");
-        },
+      async editProfile(){
+        if(this.check_roles.roles === 'user'){
+          await this.$router.push("/user/profile").catch(()=>{});
+        }else{
+          this.$router.push({ name: 'personnel_formedit', params: { id: this.check_roles.id },})
+        }
+      },
       async logout() {
           await this.$store.dispatch("logout");
           if (this.check_roles.roles === "user") {
@@ -320,7 +311,22 @@ import store from '../store/index.js';
     background-color: #167dc2!important;
     padding: 0;
   }
-  .sidebar .head{
+  .head-menuhome{
+    flex-wrap: inherit!important;
+    width: 100%;
+  }
+  .text-head{
+    color: white;
+    font-size: 17px!important;
+  }
+  .logo-cgd{
+    max-width: 48px;
+    height: 48px;
+  }
+  .logo-cgd img{
+    width: 100%;
+  }
+  /* .sidebar .head{
     height: 64px;
     display: flex;
     justify-content: center;
@@ -328,7 +334,7 @@ import store from '../store/index.js';
     color: white;
     font-size: 15px;
 
-  }
+  } */
   .navbar{
     background-color: #167dc2!important;
     color: white!important;
@@ -337,6 +343,12 @@ import store from '../store/index.js';
     color: white!important;
   }
 
+  .style-navbar {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
   .main{
     background-color: #eef0f8;
   }
@@ -412,6 +424,10 @@ import store from '../store/index.js';
       border: 1px solid #003366;
       background-color: #003366!important;
       color: white!important;
+  }
+  .f-24 {
+    font-size: 24px;
+    cursor: pointer;
   }
   .f-16{
     font-size: 14px;
