eedf7fe5180c870973a5acdd6e3adcc9c0cf6a35
Jeedev <jijiswta18@gmail.com>
2024-03-13 15:39:09 +0700

    update page

--- src/views/backoffice/report/ReportView.vue
+++ src/views/backoffice/report/ReportView.vue
@@ -1,130 +1,113 @@
 <template>
-    <div class="user-view">
-      <loaderView ref="loader"/>
-      <div class="dashboard">
+  <div class="user-view">
+    <loaderView ref="loader"/>
+    <div class="dashboard">
 
-        <v-container class="bg-surface-variant">
-          <h2 class="text-left mb-2">ภาพรวมโครงการ</h2>
-          <v-row>
-            <v-col cols="12" md="3">
-              <v-sheet class="ma-2 pa-2">
-                <div class="box-status">
-                  <h4>ทั้งหมด</h4>
-                  <p class="mb-1">{{ statusAllTotal ? statusAllTotal : 0 }} รายการ</p>
-                  <i aria-hidden="true" class="v-icon mdi mdi-clipboard-text"></i>
-                </div>
-               
-              </v-sheet>
-            </v-col>
+      <v-container class="bg-surface-variant">
+        <h2 class="text-left mb-2">ภาพรวมโครงการ</h2>
+        <v-row>
 
-            <v-col cols="12" md="3">
-              <v-sheet class="ma-2 pa-2 border-gray">
-                <div class="box-status">
-                  <h4>รอรับเรื่อง</h4>
-                  <p class="mb-1">{{statusWaitTotal ? statusWaitTotal : 0}} รายการ</p>
-                  <i aria-hidden="true" class="v-icon mdi mdi-jira"></i>
-                </div>
-               
-              </v-sheet>
-            </v-col>
+          <v-col cols="12" md="3">
+            <v-sheet class="ma-2 pa-2">
+              <div class="box-status">
+                <h4>ทั้งหมด</h4>
+                <p class="mb-1">{{ statusAllTotal ? statusAllTotal : 0 }} รายการ</p>
+                <i aria-hidden="true" class="v-icon mdi mdi-clipboard-text"></i>
+              </div> 
+            </v-sheet>
+          </v-col>
 
-            <v-col cols="12" md="3">
-              <v-sheet class="ma-2 pa-2 border-yellow">
-                <div class="box-status">
-                  <h4>รับเรื่อง</h4>
-                  <p class="mb-1">{{statusAcceptTotal ? statusAcceptTotal : 0}} รายการ</p>
-                  <i aria-hidden="true" class="v-icon mdi mdi-clipboard-check"></i>
-                </div>
-                
-              </v-sheet>
-            </v-col>
-            <v-col cols="12" md="3">
-              <v-sheet class="ma-2 pa-2 border-orange">
-                <div class="box-status">
-                  <h4>ไม่รับเรื่อง</h4>
-                  <p class="mb-1">{{statusNotAcceptTotal ? statusNotAcceptTotal : 0}} รายการ</p>
-                  <i aria-hidden="true" class="v-icon mdi mdi-clipboard-remove"></i>
-                </div>
-               
-              </v-sheet>
-            </v-col>
+          <v-col cols="12" md="3">
+            <v-sheet class="ma-2 pa-2 border-gray">
+              <div class="box-status">
+                <h4>รอรับเรื่อง</h4>
+                <p class="mb-1">{{statusWaitTotal ? statusWaitTotal : 0}} รายการ</p>
+                <i aria-hidden="true" class="v-icon mdi mdi-jira"></i>
+              </div>
+            </v-sheet>
+          </v-col>
 
-            <v-col cols="12" md="3">
-              <v-sheet class="ma-2 pa-2 border-blue">
-                <div class="box-status">
-                  <h4>อยู่ระหว่างดำเนินการ</h4>
-                  <p class="mb-1">{{statusProcessTotal ? statusProcessTotal : 0}} รายการ</p>
-                  <i aria-hidden="true" class="v-icon mdi mdi-clipboard-clock"></i>
-                </div>
-               
-              </v-sheet>
-            </v-col>
+          <v-col cols="12" md="3">
+            <v-sheet class="ma-2 pa-2 border-yellow">
+              <div class="box-status">
+                <h4>รับเรื่อง</h4>
+                <p class="mb-1">{{statusAcceptTotal ? statusAcceptTotal : 0}} รายการ</p>
+                <i aria-hidden="true" class="v-icon mdi mdi-clipboard-check"></i>
+              </div>
+            </v-sheet>
+          </v-col>
 
-            <v-col cols="12" md="3">
-              <v-sheet class="ma-2 pa-2 border-purple">
-                <div class="box-status">
-                  <h4>สอบถามข้อมูลเพิ่มเติม</h4>
-                  <p class="mb-1">{{statusAskForTotal ? statusAskForTotal : 0}} รายการ</p>
-                  <i aria-hidden="true" class="v-icon mdi mdi-clipboard-edit"></i>
-                </div>
-              
-              </v-sheet>
-            </v-col>
+          <v-col cols="12" md="3">
+            <v-sheet class="ma-2 pa-2 border-orange">
+              <div class="box-status">
+                <h4>ไม่รับเรื่อง</h4>
+                <p class="mb-1">{{statusNotAcceptTotal ? statusNotAcceptTotal : 0}} รายการ</p>
+                <i aria-hidden="true" class="v-icon mdi mdi-clipboard-remove"></i>
+              </div>
+            </v-sheet>
+          </v-col>
 
-            <v-col cols="12" md="3">
-              <v-sheet class="ma-2 pa-2 border-green">
-                <div class="box-status">
-                  <h4>เรื่องเสร็จ</h4>
-                  <p class="mb-1">{{statusFinishTotal ? statusFinishTotal : 0}} รายการ</p>
-                  <i aria-hidden="true" class="v-icon mdi mdi-check-bold"></i>
-                </div>
-               
-              </v-sheet>
-            </v-col>
-          </v-row>
+          <v-col cols="12" md="3">
+            <v-sheet class="ma-2 pa-2 border-blue">
+              <div class="box-status">
+                <h4>อยู่ระหว่างดำเนินการ</h4>
+                <p class="mb-1">{{statusProcessTotal ? statusProcessTotal : 0}} รายการ</p>
+                <i aria-hidden="true" class="v-icon mdi mdi-clipboard-clock"></i>
+              </div>
+            </v-sheet>
+          </v-col>
 
-        </v-container>
+          <v-col cols="12" md="3">
+            <v-sheet class="ma-2 pa-2 border-purple">
+              <div class="box-status">
+                <h4>สอบถามข้อมูลเพิ่มเติม</h4>
+                <p class="mb-1">{{statusAskForTotal ? statusAskForTotal : 0}} รายการ</p>
+                <i aria-hidden="true" class="v-icon mdi mdi-clipboard-edit"></i>
+              </div>
+            </v-sheet>
+          </v-col>
+
+          <v-col cols="12" md="3">
+            <v-sheet class="ma-2 pa-2 border-green">
+              <div class="box-status">
+                <h4>เรื่องเสร็จ</h4>
+                <p class="mb-1">{{statusFinishTotal ? statusFinishTotal : 0}} รายการ</p>
+                <i aria-hidden="true" class="v-icon mdi mdi-check-bold"></i>
+              </div>
+            </v-sheet>
+          </v-col>
+
+        </v-row>
+
+      </v-container>
        
-      </div>
+    </div>
             
-        <div class="style-page">
-          <h2 class="mb-3">รายงาน</h2>
+    <div class="style-page">
+      <h2 class="mb-3">รายงาน</h2>
 
-          <div class="d-flex justify-space-between align-center mb-6">
-            <span>รายงานสรุปเเจ้งเรื่องร้องเรียน</span>
+      <div class="d-flex justify-space-between align-center mb-6">
+        <span>รายงานสรุปเเจ้งเรื่องร้องเรียน</span>
+        <v-btn
+          color="secondary"
+          fab
+          x-small
+          dark
+          @click="reportComplain"
+        >
+        <i class="fa-solid fa-magnifying-glass"></i>
+        </v-btn>
+      </div>
+    </div>
 
-            <v-btn
-              color="secondary"
-              fab
-              x-small
-              dark
-              @click="reportComplain"
-            >
-            <i class="fa-solid fa-magnifying-glass"></i>
-            </v-btn>
-          </div>
-
-          <!-- <div class="d-flex justify-space-between align-center mb-6">
-            <span>รายงานสรุปเเจ้งเรื่องร้องเรียน</span>
-            <v-btn
-              color="secondary"
-              fab
-              x-small
-              dark
-            >
-            <i class="fa-solid fa-magnifying-glass"></i>
-            </v-btn>
-          </div> -->
-
-        </div>
-     </div>
+  </div>
 </template>
 
 <script>
-import axios from "axios";
-import loaderView from '@/components/loaderView.vue';
+  import loaderView from '@/components/loaderView.vue';
+  import axios from "axios";
   export default {
-  components: { loaderView },
+    components: { loaderView },
     data: () => ({
       selectedItem: 1,
       items: [
@@ -144,13 +127,16 @@ import loaderView from '@/components/loaderView.vue';
       statusAskForTotal: null,
       statusFinishTotal: null
     }),
+    
     mounted() {
       this.getCountComplain()
     },
+
     methods:{
       reportComplain(){
         this.$router.push({ name: 'report-complain'})
       },
+
       getIcon (status_call) {
         if (status_call == 0) return '#a19d9d'
         else if (status_call == 1) return '#FFA000'
@@ -160,6 +146,7 @@ import loaderView from '@/components/loaderView.vue';
         else if (status_call == 5) return 'green'
         else return 'green'
       },
+
       getColor (status_call) {
         if (status_call == 0) return '#a19d9d'
         else if (status_call == 1) return '#FFA000'
@@ -169,6 +156,7 @@ import loaderView from '@/components/loaderView.vue';
         else if (status_call == 5) return 'green'
         else return 'green'
       },
+
       getstatus (status_call) {
         if (status_call == 0) return 'รอรับเรื่อง'
         else if (status_call == 1) return 'รับเรื่อง'
@@ -178,8 +166,8 @@ import loaderView from '@/components/loaderView.vue';
         else if (status_call == 5) return 'เรื่องเสร็จ'
         else return ''
       },
-      async getCountComplain(){
 
+      async getCountComplain(){
         try {
           let path                  = await `/api/getCountComplain`
           let response              = await axios.get(`${path}`) 
@@ -218,54 +206,49 @@ import loaderView from '@/components/loaderView.vue';
 
       }
     }
+
   }
 </script>
+
 <style>
-/* .dashboard{
-  display: flex;
-} */
-.dashboard .card {
+
+  .dashboard .card {
     background-color: #fff;
     padding: 20px;
     margin-bottom: 20px;
-    /* margin: 0.5rem; */
     border-radius: 4px;
     box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
     max-width: 33.33%;
-}
-.dashboard .v-sheet {
-  border-radius: 5px;
-  box-shadow: 0 3px 1px -2px rgba(0,0,0,.2), 0 2px 2px 0 rgba(0,0,0,.14), 0 1px 5px 0 rgba(0,0,0,.12);
-}
-.box-status .v-icon{
+  }
+  .dashboard .v-sheet {
+    border-radius: 5px;
+    box-shadow: 0 3px 1px -2px rgba(0,0,0,.2), 0 2px 2px 0 rgba(0,0,0,.14), 0 1px 5px 0 rgba(0,0,0,.12);
+  }
+  .box-status .v-icon{
     width: 30px;
     height: 30px;
     font-size: 18px!important;
     border-radius: 100%;
     background: #003366;
     color: white;
-}
-.border-gray{
-  border: 3px solid #a19d9d!important;
-}
-.border-yellow{
-  border: 3px solid #FFA000!important;
-}
-.border-orange{
-  border: 3px solid #EF6C00!important;
-}
-.border-blue{
-  border: 3px solid #01579B!important;
-}
-.border-purple{
-  border: 3px solid #512DA8!important;
-}
-.border-green{
-  border: 3px solid green!important;
-}
+  }
+  .border-gray{
+    border: 3px solid #a19d9d!important;
+  }
+  .border-yellow{
+    border: 3px solid #FFA000!important;
+  }
+  .border-orange{
+    border: 3px solid #EF6C00!important;
+  }
+  .border-blue{
+    border: 3px solid #01579B!important;
+  }
+  .border-purple{
+    border: 3px solid #512DA8!important;
+  }
+  .border-green{
+    border: 3px solid green!important;
+  }
 
-/* .card h3 {
-    font-size: 18px;
-    margin-bottom: 10px;
-} */
 </style>
